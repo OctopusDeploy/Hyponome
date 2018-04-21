5daf93a5fd70085d9943cd50154a8c8b8335d82f
Henrik Andersson <henrik.andersson@octopus.com>
2018-04-22 09:45:22 +1000

    Add informational version too

--- src/Hyponome.Web/Properties/AssemblyInfo.cs
+++ src/Hyponome.Web/Properties/AssemblyInfo.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
