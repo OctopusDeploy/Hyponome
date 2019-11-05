7a05fb781809b765a014fa476f5d10d2dedca751
Henrik Andersson <henrik.andersson@octopus.com>
2019-11-05 10:42:22 +1000

    Add assembly info for versioning

---
+++ source/Hyponome/Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyDescription("The Octopus Community Library Pull Request review tool portal")]
+[assembly: AssemblyCompany("Octopus Deploy")]
+[assembly: AssemblyProduct("Octopus Community Library Pull Request review tool")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2019")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Hyponome.Client")]
---
+++ source/Hyponome/Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Octopus Deploy")]
+[assembly: AssemblyProduct("Octopus Community Library Pull Request review tool")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2019")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Hyponome.Shared")]
